b78b7ec78d45aa037708d9ca0da6632b537f3537
Mikhail Beloborodov <kiritohenjanai@gmail.com>
2021-12-09 16:48:01 +0400

    added first variable int

--- test.cpp
+++ test.cpp
@@ -1,10 +1,21 @@
+//including standard c++ library
 #include <iostream>
 
+//using a namespace std in this program
 using namespace std;
 
 int main()
 {
-	cout << "Hello World!";
+	//initialize a variable and give it a number;
+	int number;
+	number = 2021;
+
+	//print result on the screen
+	cout << "The year is: ";
+	cout << number;
+
+	//returns 0 if program is fine, 1 if there is an error.
 	return 0;
 }
+
 
